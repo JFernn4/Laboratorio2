31b097aad7362a746f99d3b192e95d36924f8036
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-07 09:05:50 -0600

    Clases "Suite" y "HabitacionDeluxe"

---
+++ Laboratorio2/HabitacionDeluxe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    internal class HabitacionDeluxe : Habitacion
+    {
+        public string ServiciosExtras {  get; set; }
+
+        public HabitacionDeluxe(int numero, double precioPorNoche, bool disponible, string clienteAsignado, string serviciosExtras) : base (numero, precioPorNoche, disponible, clienteAsignado)
+        {
+            ServiciosExtras = serviciosExtras;
+        }
+    }
+}
---
+++ Laboratorio2/Suite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio2
+{
+    internal class Suite : Habitacion
+    {
+        public int NumeroDeHabitaciones {  get; set; }
+        public bool TieneJacuzzi {  get; set; }
+
+        public Suite(int numero, double precioPorNoche, bool disponible, string clienteAsignado, int numeroDeHabitaciones, bool tieneJacuzzi) : base (numero, precioPorNoche, disponible, clienteAsignado)
+        {
+            NumeroDeHabitaciones = numeroDeHabitaciones;
+            TieneJacuzzi = tieneJacuzzi;
+        }
+    }
+}
