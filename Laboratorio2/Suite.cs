0d5536d523b575c5c031fa2b4fab386fdf287357
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-07 09:57:09 -0600

    Función agregar suite

--- Laboratorio2/Suite.cs
+++ Laboratorio2/Suite.cs
@@ -16,6 +16,39 @@ namespace Laboratorio2
             NumeroDeHabitaciones = numeroDeHabitaciones;
             TieneJacuzzi = tieneJacuzzi;
         }
-        public static void 
+        public static void AgregarSuite (List<Habitacion> listaHabitaciones)
+        {
+            bool continuar = true;
+            bool tieneJacuzzi=true;
+            Console.WriteLine("Ingrese el número de la habitación.");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el precio por noche de la habitación.");
+            double precioPorNoche = Convert.ToDouble(Console.ReadLine());
+            bool disponible = true; //Al agregar una nueva habitación se asume que está vacía
+            string clienteAsignado = "Vacío."; //Si está vacía significa que no tiene cliente asignado por defecto.
+            Console.WriteLine("Ingrese el número de habitaciones de la suite.");
+            int numeroDeHabitaciones= Convert.ToInt32(Console.ReadLine());
+            while (continuar)
+            {
+                Console.Clear();
+                Console.WriteLine("Si tiene jacuzzi, presione 1. Si no, presione 2.");
+                int opcionJacuzzi= Convert.ToInt32(Console.ReadLine());
+                if (opcionJacuzzi==1)
+                {
+                    tieneJacuzzi = true;
+                    continuar = false;
+                }
+                else if (opcionJacuzzi==2)
+                {
+                    tieneJacuzzi= false;
+                    continuar = false;
+                }
+                else
+                {
+                    Console.WriteLine("Dedbe ingresar 1 o 2.");
+                    Console.ReadKey();
+                }
+            }
+        }
     }
 }
